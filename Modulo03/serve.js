3e00651903743296054d2b4c22164d2972c5cd5c
Marcos Guilherme <marcosguilherme.ti@gmail.com>
2020-11-22 22:48:26 -0300

    modulo03

--- Modulo03/serve.js
+++ Modulo03/serve.js
@@ -15,17 +15,53 @@ nunjucks.configure("views", {
 })
 
 server.get('/', function(req, res) {
-    return res.render("index")
+    const index = {
+        image_logo: "https://pbs.twimg.com/profile_images/1291682473592659968/sEorc6oh.jpg",
+        name: "Rocketseat",
+        link_name: "https://rocketseat.com.br/",
+        description: "As melhores tecnologias em programação, direto ao ponto e do jeito certo. No meio de tanta informação e da quantidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa.",
+        description_techs: "Principais Cursos Ofertados",
+        techs: [
+
+            { name: "Node.js" },
+            { name: "React.js" },
+            { name: "JavaScript" },
+        ],
+    }
+
+    return res.render("index", { index })
+})
+
+server.get('/sobre', function(req, res) {
+    const sobre = {
+
+        image_logo: "https://pbs.twimg.com/profile_images/1291682473592659968/sEorc6oh.jpg",
+        name: "Rocketseat",
+        link_name: "https://rocketseat.com.br/",
+        inicio: "Nossas atividades começaram em 2010",
+        description: "Principais Cursos Ofertados",
+        cursos: [
+
+            { name: "Starter - Totalmente Gratuito" },
+            { name: "Lauch Base - Pago" },
+            { name: "Go Stack - Pago" },
+        ],
+
+    }
+
+    return res.render("sobre", { sobre })
 })
 
 server.get('/conteudos', function(req, res) {
-    return res.render("conteudos")
+    return res.render("conteudos", { items: content })
 })
-server.get('/sobre', function(req, res) {
-    return res.render("sobre")
-})
+
 server.use(function(req, res) {
-    res.status(404).render("not-foud")
+    const error = {
+        image_error: "https://image.freepik.com/vetores-gratis/glitch-error-404-page-background_23-2148072533.jpg"
+    }
+
+    res.status(404).render("not-foud", { error })
 })
 
 server.listen(4000, function() {
